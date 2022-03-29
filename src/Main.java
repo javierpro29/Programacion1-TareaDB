e5ba5555ee548e59afeacbb419e56409ab62780e
javierpro29 <andersonjavier2909@gmail.com>
2022-03-29 12:55:39 -0400

    add panels
    agregando los paneles principales
    en main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.SystemColor;
 
 public class Main {
 
@@ -37,6 +41,19 @@ public class Main {
 		frame.setSize(1080, 720);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(Color.GRAY);
+		panel_2.setBounds(273, 0, 791, 681);
+		panel.add(panel_2);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(SystemColor.textHighlight);
+		panel_1.setBounds(0, 0, 273, 681);
+		panel.add(panel_1);
 	}
-
 }
